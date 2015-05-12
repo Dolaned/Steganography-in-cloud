4900a33414c07df838f35b23e4d7d365b65a5642
Dylan Aird <dylan.aird595@gmail.com>
2015-05-13 01:03:54 +1000

    Working on amazon pull and push!

--- Stego/src/AmazonHandler.java
+++ Stego/src/AmazonHandler.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 
 import com.amazonaws.AmazonClientException;
 import com.amazonaws.AmazonServiceException;
@@ -9,44 +10,67 @@ import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 public class AmazonHandler {
 
-		public boolean pushToCloud(String bucketName, String accessKey, File files){
+		public void pushToCloud(String bucketName, String accessKey, File[] files){
 			
 	        AmazonS3 s3client = new AmazonS3Client(new ProfileCredentialsProvider());
-	        try {
-	            System.out.println("Uploading a new object to S3 from a file\n");
-	            s3client.putObject(new PutObjectRequest(
-	            		                 bucketName, accessKey, files));
+	        for(int i = 0; i < files.length; i++){
+		        try {
+		        	//print out the name of the file and bucket to upload too.
+		            System.out.println("Uploading: " + files[i].getName() + " to S3 bucket: " + bucketName +" \n");
+		            // get the current file iteration
+		            File f = files[i];
+		            
+		            //checks if the amazon bucket name exists.
+		            if(s3client.doesBucketExist(bucketName)){
+		            	if(s3client.putObject(new PutObjectRequest(
+       		                 bucketName, accessKey, f)) != null){
+		            		System.out.println("File Uploaded: " + f.getName() + " File "+ (i+1) +" of "+ files.length);
+		            	}
+		            	
+		            }else if(s3client.createBucket(bucketName) != null){
+		            	if(s3client.putObject(new PutObjectRequest(
+	       		                 bucketName, accessKey, f)) != null){
+		            		System.out.println("File Uploaded: "+ f.getName()+ " File "+ (i+1) +" of "+ files.length);
+		            	}
+		            }else{
+		            	System.out.println("Could not create bucket!");
+		            }
 
-	         } catch (AmazonServiceException ase) {
-	            System.out.println("Caught an AmazonServiceException, which " +
-	            		"means your request made it " +
-	                    "to Amazon S3, but was rejected with an error response" +
-	                    " for some reason.");
-	            System.out.println("Error Message:    " + ase.getMessage());
-	            System.out.println("HTTP Status Code: " + ase.getStatusCode());
-	            System.out.println("AWS Error Code:   " + ase.getErrorCode());
-	            System.out.println("Error Type:       " + ase.getErrorType());
-	            System.out.println("Request ID:       " + ase.getRequestId());
-	            return false;
-	        } catch (AmazonClientException ace) {
-	            System.out.println("Caught an AmazonClientException, which " +
-	            		"means the client encountered " +
-	                    "an internal error while trying to " +
-	                    "communicate with S3, " +
-	                    "such as not being able to access the network.");
-	            System.out.println("Error Message: " + ace.getMessage());
-	            return false;
+	
+		         } catch (AmazonServiceException ase) {
+		            System.out.println("Caught an AmazonServiceException, which " +
+		            		"means your request made it " +
+		                    "to Amazon S3, but was rejected with an error response" +
+		                    " for some reason.");
+		            System.out.println("Error Message:    " + ase.getMessage());
+		            System.out.println("HTTP Status Code: " + ase.getStatusCode());
+		            System.out.println("AWS Error Code:   " + ase.getErrorCode());
+		            System.out.println("Error Type:       " + ase.getErrorType());
+		            System.out.println("Request ID:       " + ase.getRequestId());
+		        } catch (AmazonClientException ace) {
+		            System.out.println("Caught an AmazonClientException, which " +
+		            		"means the client encountered " +
+		                    "an internal error while trying to " +
+		                    "communicate with S3, " +
+		                    "such as not being able to access the network.");
+		            System.out.println("Error Message: " + ace.getMessage());
+		        }
+				
 	        }
-			
-			return true;
 		}
 		
-		public boolean pullFromCloud(String bucketName, String accessKey, File files){
+		public ArrayList<File> pullFromCloud(String bucketName, String accessKey){
+			
+			//init amazon client.
 	        AmazonS3 s3client = new AmazonS3Client(new ProfileCredentialsProvider());
+	        
+	        
 	        try {
-	            System.out.println("Uploading a new object to S3 from a file\n");
-	            s3client.putObject(new PutObjectRequest(
-	            		                 bucketName, accessKey, files));
+	        	if(s3client.doesBucketExist(bucketName)){
+	        		System.out.println(s3client.listObjects(bucketName));
+	        	}else{
+	        		System.out.println("Your bucket doesnt Exist, choose from the following buckets.");
+	        	}
 
 	         } catch (AmazonServiceException ase) {
 	            System.out.println("Caught an AmazonServiceException, which " +
@@ -58,7 +82,6 @@ public class AmazonHandler {
 	            System.out.println("AWS Error Code:   " + ase.getErrorCode());
 	            System.out.println("Error Type:       " + ase.getErrorType());
 	            System.out.println("Request ID:       " + ase.getRequestId());
-	            return false;
 	        } catch (AmazonClientException ace) {
 	            System.out.println("Caught an AmazonClientException, which " +
 	            		"means the client encountered " +
@@ -66,9 +89,8 @@ public class AmazonHandler {
 	                    "communicate with S3, " +
 	                    "such as not being able to access the network.");
 	            System.out.println("Error Message: " + ace.getMessage());
-	            return false;
 	        }
+			return null;
 			
-			return true;
 		}
 }
